1b86fa9882ba7caf81be9eee660b111b806d0117
alexafleming <alexaflemingmn@gmail.com>
2023-10-03 15:14:57 -0500

    created deatils component

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+function Details() {
+    
+  
+  
+    return (
+      <div>
+        <h2>Details</h2>
+      </div>
+    );
+  }
+  
+  export default Details;
