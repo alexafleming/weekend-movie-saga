047b62bc774b3a4b8443e55c3ae75ad317af8f05
alexafleming <alexaflemingmn@gmail.com>
2023-10-03 15:22:12 -0500

    got route created for /details

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 
 function Details() {
-    
+    const { id } = useParams();
   
   
     return (
       <div>
         <h2>Details</h2>
+        <p>Movie ID: {id}</p>
       </div>
     );
   }
