26f27bc0e26b9946c5be3e8117ae1d6523cedfdb
alexafleming <alexaflemingmn@gmail.com>
2023-10-05 16:03:15 -0500

    fixed another bug. backspaced a few to many times on my code and delted the back button got it up and reworking

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 function Details() {
@@ -8,6 +9,7 @@ function Details() {
     const dispatch = useDispatch();
     const movie = useSelector(store => store.movieDetail)
     const movieGenres = useSelector(store => store.genres)
+    const history = useHistory();
     
     useEffect(() => {
         dispatch({type: 'FETCH_DETAILS', payload: id })
