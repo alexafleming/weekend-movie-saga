ff71e92ca4eb94adc0d8df359a420486ef9f2c6f
alexafleming <alexaflemingmn@gmail.com>
2023-10-05 15:59:16 -0500

    fixed a console error

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,21 +2,19 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import Details from '../Details/Details';
+import * as React from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
       <Router>        
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
-        {/* Details page */}
         <Route path="/details/:id" exact>
           <Details />
         </Route>
-        {/* Add Movie page */}
       </Router>
     </div>
   );
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,38 +1,42 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import './MovieList.css'
+import { Card, CardContent, Typography, Grid } from '@mui/material';
+import './MovieList.css';
 
 function MovieList() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const movies = useSelector((store) => store.movies);
 
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+  useEffect(() => {
+    dispatch({ type: 'FETCH_MOVIES' });
+  }, []);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
-    
-    const movieDetails = (id) => {
-        history.push(`/details/${id}`)
-    };
+  const movieDetails = (id) => {
+    history.push(`/details/${id}`);
+  };
 
-    return (
-        <main>
-            <h1>Movie List</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div onClick={() => movieDetails(movie.id)} key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
+  return (
+    <main>
+      <h1>MOVIE LIST</h1>
 
-    );
+      <Grid container spacing={2} className="movies">
+        {movies.map((movie) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
+            <Card onClick={() => movieDetails(movie.id)} className="movie-card">
+              <img src={movie.poster} alt={movie.title} className="card-image" />
+              <CardContent>
+              <Typography variant="h6" component="div" className="movie-title">
+                  {movie.title}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
 }
 
 export default MovieList;
